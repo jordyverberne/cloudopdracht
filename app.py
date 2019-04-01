52e2e292c69ebf5203aa394bf079b67fdf506fcc
root <root@lenovo.localdomain>
2019-04-01 15:18:34 +0200

    changed to port 443

--- app.py
+++ app.py
@@ -86,5 +86,5 @@ class checkInOut(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context='adhoc', host='0.0.0.0', port=5000)
+    app.run(debug=True, ssl_context='adhoc', host='0.0.0.0', port=443)
 
